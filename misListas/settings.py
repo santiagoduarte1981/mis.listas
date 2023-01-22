458d87d1b6bfa29c075d4df419b56cedbc01f3d4
sanduartedev <sanduartedev@gmail.com>
2023-01-22 15:19:14 -0300

    A ver

--- misListas/settings.py
+++ misListas/settings.py
@@ -72,14 +72,9 @@ WSGI_APPLICATION = 'misListas.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': config('NAME_DB'),
-        'USER': config('USER_DB'),
-        'PASSWORD': config('PASSWORD_DB'),
-        'HOST': config('HOST_DB'),
-        'PORT': '3306',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-}
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
